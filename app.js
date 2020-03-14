e8cd838ccb292afbb86992b15c04b7b16924f335
Safayet Jamil <work.jamil4@gmail.com>
2020-03-14 20:01:57 +0600

    v0.04

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+if(process.env.NODE_ENV ==='development'){
+  app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
